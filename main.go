7c498eba194829298c3ef66f09c5fec2cb4deadc
Ryo Shen <shenlw66@gmail.com>
2022-02-26 15:08:19 +0800

    config file

--- main.go
+++ main.go
@@ -3,14 +3,34 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
+	"os"
 	"slw.app/ginessential/common"
 )
 
 func main() {
+	InitConfig()
+
 	db := common.InitDB()
 	defer db.Close()
 
 	r := gin.Default()
 	r = CollectRouter(r)
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	port := viper.GetString("server.port")
+	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	if port != "" {
+		panic(r.Run(":" + port))
+	}
+	panic(r.Run())
+}
+
+func InitConfig() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 }
